52b722b815817decd47cca8810d6357c2a2c930e
Shimada666 <Shimada666@users.noreply.github.com>
2022-07-25 08:18:59 +0000

    Automated deployment: Mon Jul 25 08:18:59 UTC 2022 Shimada666/MyBlog@fd15ff1ed28f9b842c81227489f962c16e34013d

--- assets/index.md.6200b2c7.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o as a}from"./app.776ca857.js";const m='{"title":"Home","description":"","frontmatter":{"home":true,"footer":"MIT Licensed | Copyright \xA9 2021 Shimada666"},"headers":[],"relativePath":"index.md","lastUpdated":1658665049000}',o={};function r(n,s,c,d,i,p){return a(),t("div")}var f=e(o,[["render",r]]);export{m as __pageData,f as default};
